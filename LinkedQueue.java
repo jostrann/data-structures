dc15780db40e1d259ca1af2e2c6a435ef1a1aa6d
trancode <65465480+trancode@users.noreply.github.com>
2022-01-27 13:24:06 -0500

    Update LinkedQueue.java

--- LinkedQueue.java
+++ LinkedQueue.java
@@ -7,36 +7,27 @@ public class LinkedQueue {
 	public boolean add(String data) {
 		Node nn = new Node();
 		nn.setData(data);
-		
-		// If the queue was empty it now contains 1 element, both head and tail should
-		// refer to it.
 		if (count == 0) {
 			head = tail = nn;
 			count = 1;
 			return true;
 		}
-		
 		tail.setNext(nn);
 		tail = nn;
 		count++;
-		
 		return true;
 	}
 
 	public String remove() {
 		if (isEmpty()) {
 			return null;
-		}
-		
+		}	
 		String rtn = head.getData();
 		head = head.getNext();
 		count--;
-		
-		// If we removed the last thing in the queue
 		if (count == 0) {
 			tail = null;
 		}
-		
 		return rtn;
 	}
 
@@ -48,15 +39,12 @@ public class LinkedQueue {
 		if (isEmpty()) {
 			return null;
 		}
-		
-		// TODO Auto-generated method stub
 		return head.getData();
 	}
 
 	public void clear() {
 		head = tail = null;
 		count = 0;
-		
 	}
 
 	public boolean isFull() {
@@ -66,5 +54,4 @@ public class LinkedQueue {
 	public boolean isEmpty() {
 		return (count == 0);
 	}
-
 }
